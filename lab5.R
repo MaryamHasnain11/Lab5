e4dd8e940f2e7f56c0c863484351481c21b1d1a5
MaryamHasnain11 <mhasnain.bese15seecs@seecs.edu.pk>
2018-03-07 01:02:01 -0800

    v3

--- lab5.R
+++ lab5.R
@@ -1,7 +1,5 @@
 barplot(mtcars$mpg,mtcars$hp, col='blue', xlab='mpg', ylab='hp', main='mpg')
 plot(mtcars$mpg,mtcars$disp)
-
-
-
-
+barplot(mtcars$wt,mtcars$hp, col='black', xlab='wt', ylab='hp', main='wt')
+hist(mtcars$gear,col="red",main="Histogram for  gears",xlab="Number of gears",ylab="Number of cars")
 
